5c3839f84394fd864f9a66250da7096f10040e79
Alexey <Shaht3r@gmail.com>
2020-02-22 23:11:22 +0300

    feat: aad task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+	if (arguments.length === 0) return [];
+	if (matrix.length === 0) return [];
+	
+	matrix.forEach((arr, index) => {
+		if (index % 2 !== 0) {
+			arr.reverse();
+		}
+	});
+	return [].concat(...matrix);
 }
